b53659245f4430443caf5d4a7ff99a9b4d14db78
Mian Chen <mianchen@gmail.com>
2023-11-17 22:59:24 +0000

    unique company name

--- company/models.py
+++ company/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Company(models.Model):
     """Company model"""
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
 
     def __str__(self) -> str:
