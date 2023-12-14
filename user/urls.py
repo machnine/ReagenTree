3d2645d30357ec653355b110d677efd94e824906
Mian Chen <mianchen@gmail.com>
2023-12-14 15:00:44 +0000

    user profile views

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,18 @@
 """url patterns for user app"""
 from django.urls import path
 
-from .views import UserRegistrationView, UserLoginView, UserLogoutView, UserProfileView, UserProfileEdit
+from .views import (
+    UserRegistrationView,
+    UserLoginView,
+    UserLogoutView,
+    UserProfileView,
+    UserProfileEdit,
+)
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("user/profile/", UserProfileView.as_view(), name="user_profile"),
-    path("user/edit/", UserProfileEdit.as_view(), name="user_edit"),
+    path("user/update/", UserProfileEdit.as_view(), name="user_update"),
 ]
