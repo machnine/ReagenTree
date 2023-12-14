61e4bba114267d7c508f16fcae23c2a06e2dc5c0
Mian Chen <mianchen@gmail.com>
2023-12-14 18:19:58 +0000

    minor change

--- item/templatetags/stock_tags.py
+++ item/templatetags/stock_tags.py
@@ -10,14 +10,16 @@ def delivery_condicon(condition):
     """Return a coloured Bootstrap icon for delivery condition"""
     icons = {
         0: mark_safe(
-            '<i class="bi bi-question-diamond text-secondary" title="Condition: Unknown"></i>'
+            '<i class="bi bi-question-diamond text-secondary" data-bs-toggle="tooltip" title="Condition: Unknown"></i>'
         ),
-        1: mark_safe('<i class="bi bi-hand-thumbs-up text-success" title="Condition: Good"></i>'),
+        1: mark_safe(
+            '<i class="bi bi-hand-thumbs-up text-success" data-bs-toggle="tooltip" title="Condition: Good"></i>'
+        ),
         2: mark_safe(
-            '<i class="bi bi-hand-thumbs-down-fill text-danger" title="Condition: Unacceptable"></i>'
+            '<i class="bi bi-hand-thumbs-down-fill text-danger" data-bs-toggle="tooltip" title="Condition: Unacceptable"></i>'
         ),
         3: mark_safe(
-            '<i class="bi bi-exclamation-diamond text-warning" title="Condition: Attention required!"></i>'
+            '<i class="bi bi-exclamation-diamond text-warning" data-bs-toggle="tooltip" title="Condition: Attention required!"></i>'
         ),
     }
     return icons.get(condition, icons[0])
