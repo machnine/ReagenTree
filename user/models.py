9fb9748801565705bb4685723b777e79abbbd060
Mian Chen <mianchen@gmail.com>
2024-01-06 13:46:49 +0000

    user initial unique

--- user/models.py
+++ user/models.py
@@ -8,4 +8,4 @@ class CustomUser(AbstractUser):
     Custom user model: best practice to extend the AbstractUser
     to create this model for future exensiability.
     """
-    initials = models.CharField(max_length=10, blank=True)
+    initials = models.CharField(max_length=10, blank=True, unique=True)
