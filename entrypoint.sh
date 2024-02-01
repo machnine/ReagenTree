b7e3cf68c6912069db687bdf5b591f0e81a5ce25
Mian Chen <mianchen@gmail.com>
2024-02-01 11:46:10 +0000

    removed docker

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Apply database migrations
-python manage.py migrate
-
-# Run database initialization script
-python manage.py initial_data
-# Collect static files
-python manage.py collectstatic --noinput
