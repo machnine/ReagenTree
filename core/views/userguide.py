739a4a4d1bc02455bcb037c4c704deec327ed594
Mian Chen <mianchen@gmail.com>
2024-06-11 13:52:00 +0100

    user guide loginrequired

--- core/views/userguide.py
+++ core/views/userguide.py
@@ -1,9 +1,10 @@
 """user guide page views """
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class UserGuideView(TemplateView):
+class UserGuideView(LoginRequiredMixin, TemplateView):
     """User guide page view."""
 
     template_name = "userguide.html"
